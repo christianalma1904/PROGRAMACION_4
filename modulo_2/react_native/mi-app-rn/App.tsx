482878a1a73aae84a2bece4356f580b7df0e23ad
Daniel Alcocer <christianalma001@gmail.com>
2025-10-27 22:12:37 -0500

    Semana 5

---
+++ modulo_2/react_native/mi-app-rn/App.tsx
@@ -0,0 +1,28 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import SaludoProps from './src/components/SaludoProps';
+import Saludo from './src/components/Saludo';
+import Contador from './src/components/Contador';
+import Caja from './src/components/Caja';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Saludo />
+      <Text>Open up App.tsx to start working on your app!</Text>
+      <StatusBar style="auto" />
+      <SaludoProps nombre='Christian' />
+      <Contador />
+      <Caja />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
