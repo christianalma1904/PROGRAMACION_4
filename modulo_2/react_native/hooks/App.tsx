dcc4b730f5791d06944d0f841142d0bc905c64d3
Daniel Alcocer <christianalma001@gmail.com>
2025-10-29 19:42:33 -0500

    Semana 5 v2

---
+++ modulo_2/react_native/hooks/App.tsx
@@ -0,0 +1,9 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import AppNavigator from './src/screens/AppNavigator';
+
+export default function App() {
+  return (
+    <AppNavigator />
+  );
+};
